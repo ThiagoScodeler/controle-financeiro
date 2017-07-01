c96520ff82ff1ef03e89483cb99cb8f70056b351
Rafael William da Silva <rws1602@gmail.com>
2017-07-01 19:29:44 -0300

    Donut chart

--- data/morris-data.js
+++ data/morris-data.js
@@ -1,8 +1,10 @@
-$(function() {
+$(function () {
 
-    infos = getDailyInfos();
-    console.log(infos);
-
+    series = getTotal();
+    console.log( series);
+    //infos = getDailyInfos();
+    //console.log(infos);
+/*
     Morris.Area({
         element: 'morris-area-chart',
         data: infos,
@@ -13,7 +15,13 @@ $(function() {
         hideHover: 'auto',
         resize: true
     });
+*/
+    Morris.Donut({
+        element: 'donut-chart-report',
+        data: series
+    });
 
+/*
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
@@ -51,21 +59,56 @@ $(function() {
         hideHover: 'auto',
         resize: true
     });
-    
+*/
 });
 
-
-function getDailyInfos(){
+function getDailyInfos() {
 
     results = [];
 
     var d = new Date();
-    var month = d.getMonth()+1;
+    var month = d.getMonth() + 1;
 
     const urlBase = 'http://localhost:3000/api/reports/movimento-by-day/';
-    url = urlBase+ month;
-    $.get(url, function(data){
+    url = urlBase + month;
+    $.get(url, function (data) {
         return data;
+    });
+    return results;
+}
+
+
+function getTotal() {
+
+    results = [];
+
+    //const url = 'http://localhost:3000/api/reports/check-saldo';
+    const url = 'http://54.233.186.50:3000/api/reports/check-saldo';
+
+    $.ajax({
+     async: false,
+     type: 'GET',
+     url: url,
+     success: function(data) {
+         if (data.entrada != null) {
+            entrada = new Object();
+            entrada.label= 'Entrada';
+            entrada.value =  parseInt(data.entrada.total);
+            results.push(entrada);
+        }
+        if (data.pagar != null) {
+            pagar = new Object();
+            pagar.label = 'Saida';
+            pagar.value =  parseInt(data.pagar.total);
+            results.push(pagar);        
+        }
+        if (data.saida != null) {
+            saida =  new Object();
+            saida.label = 'Pagar';
+            saida.value =  parseInt(data.saida.total);
+            results.push(saida);     
+        }
+     }
     });
     return results;
 }
